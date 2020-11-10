beea0f0bdbab37f542b9d6486c9302a3ee54800c
Leandro <leandrocarlini@gmail.com>
2020-11-10 16:34:14 -0300

    Adding console project

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace VogCodeChallenge.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(QuestionClass);
+            FieldInfo info = type.GetField("NamesList", BindingFlags.NonPublic | BindingFlags.Static);
+            object namesList = info.GetValue(null);
+
+            foreach (var name in (List<string>)namesList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static class QuestionClass
+        {
+            static List<string> NamesList = new List<string>()
+            {
+                "Jimmy",
+                "Jeffrey",
+                "John",
+            };
+        }
+    }
+}

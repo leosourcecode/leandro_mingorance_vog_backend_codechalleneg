ff1b8b8d03d1b657bd5788cc410b5f64c28d2395
Leandro <leandrocarlini@gmail.com>
2020-11-10 17:09:05 -0300

    Adding TESTModule

--- VogCodeChallenge.Console/Program.cs
+++ VogCodeChallenge.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace VogCodeChallenge.Console
@@ -14,8 +15,22 @@ namespace VogCodeChallenge.Console
 
             foreach (var name in (List<string>)namesList)
             {
-                Console.WriteLine(name);
+                //Console.WriteLine(name);
             }
+
+            TESTModule(-1);
+            TESTModule(0);
+            TESTModule(1);
+            TESTModule(2);
+            TESTModule(3);
+            TESTModule(4);
+            TESTModule(5);
+            TESTModule(1.0f);
+            TESTModule(2.0f);
+            TESTModule(3.0f);
+            TESTModule("s");
+            TESTModule(null);
+            TESTModule(10.0d);
         }
 
         public static class QuestionClass
@@ -26,6 +41,28 @@ namespace VogCodeChallenge.Console
                 "Jeffrey",
                 "John",
             };
+        }
+
+        public static object TESTModule(object obj)
+        {
+            if (obj == null) return obj;
+
+            switch (Type.GetTypeCode(obj.GetType()))
+            {
+                case TypeCode.Int32:
+                    if((int)obj >= 1 && (int)obj <= 4) return (int)obj * 2;
+                    return (int)obj > 4 ? (int)obj * 3 : throw new ValidationException("The input value of the integer type must be greater than 0!");
+
+                case TypeCode.Single:
+                    return (float)obj == 1.0f && (float)obj == 2.0f ? 3.0f : obj;
+
+                case TypeCode.String:
+                    return ((string)obj).ToUpper();
+
+                default:
+                    return obj;
+
+            }
         }
     }
 }
